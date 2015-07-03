37ed75b14bf21aad55bbb7d98e767831227c9175
satsukies <satsukies@gmail.com>
2015-07-03 13:32:01 +0900

    サンプルコード取り込み
    http://blog.livedoor.jp/tmako123-programming/archives/39486172.html　を参考に
    。

--- app/src/main/java/net/ddns/satsukies/udptest/MainActivity.java
+++ app/src/main/java/net/ddns/satsukies/udptest/MainActivity.java
@@ -1,17 +1,103 @@
 package net.ddns.satsukies.udptest;
 
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    Button btn1, btn2;
+    TextView txt1, txt2;
+
+    StringBuffer mBuffer = new StringBuffer(128);
+    int n;
+    DatagramSocket mSocket;
+    DatagramPacket mPacket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Handler mHandler = new Handler();
+
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        txt1 = (TextView) findViewById(R.id.txt1);
+        txt2 = (TextView) findViewById(R.id.txt2);
+
+        final String remoteIP = "192.168.11.255";
+        final int portNum = 12345;
+
+        // Button1 がクリックされた時に呼び出されるコールバックを登録
+        btn1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                n++;
+                // 文字列を作る
+                mBuffer.append(n);
+                txt1.setText(mBuffer.toString());  // tv には final が必要
+                mBuffer.delete(0, 99);
+                //toneGenerator.startTone(ToneGenerator.TONE_PROP_BEEP);
+
+                // UDP　送信
+                (new Thread(new Runnable() {
+                    public void run() {
+                        try {
+                            InetAddress host = InetAddress.getByName(remoteIP);
+                            //String message = "send by Android " + n + " \n";  // 送信メッセージ
+                            String message = "100 100";  // 送信メッセージ
+                            mSocket = new DatagramSocket();  //DatagramSocket 作成
+                            byte[] data = message.getBytes();
+                            mPacket = new DatagramPacket(data, data.length, host, portNum);  //DatagramPacket 作成
+                            mSocket.send(mPacket);
+                            mHandler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    txt2.setText("送信完了しました");
+                                }
+                            });
+                        } catch (Exception e) {
+                            System.err.println("Exception : " + e);
+                            txt2.setText("送信失敗しました");
+                        }
+                    }
+                })).start();
+            }
+        });
+
+        // カウンタの減少
+        btn2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                n--;
+                mBuffer.append(n);
+                txt1.setText(mBuffer.toString());
+                mBuffer.delete(0, 99);
+                //toneGenerator.startTone(ToneGenerator.TONE_PROP_BEEP);
+            }
+        });
+
+        n = 0;        // カウント値の初期値
+        try {
+            InetAddress host = InetAddress.getByName(remoteIP);      // IPアドレス
+            String message = "send by Android";  // 送信メッセージ
+            mSocket = new DatagramSocket();  //DatagramSocket 作成
+            byte[] data = message.getBytes();
+            mPacket = new DatagramPacket(data, data.length, host, portNum);  //DatagramPacket 作成
+            txt2.setText("初期化が完了しました");
+        } catch (Exception e) {
+            System.err.println("Exception : " + e);
+            txt2.setText("初期化に失敗しました");
+        }
     }
 
     @Override
