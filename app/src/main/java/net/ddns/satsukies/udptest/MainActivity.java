b345cf791ebdc31ec8706e3fb01e10d039f4048a
satsukies <satsukies@gmail.com>
2015-07-03 15:13:57 +0900

    touch位置をUDPで送信
    byte-arrayを使ってUDPデータの送信。SurfaceViewを触るとその位置をネットワーク全体に対して流す。

--- app/src/main/java/net/ddns/satsukies/udptest/MainActivity.java
+++ app/src/main/java/net/ddns/satsukies/udptest/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,65 +15,47 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
+import static java.lang.System.*;
+
 
 public class MainActivity extends ActionBarActivity {
 
     Button btn1, btn2;
     TextView txt1, txt2;
+    SurfaceView mSurfaceView;
 
-    StringBuffer mBuffer = new StringBuffer(128);
+    //Processingに合わせてbyte型にする
+    byte[] sendBuffer = new byte[8];
+
     int n;
     DatagramSocket mSocket;
     DatagramPacket mPacket;
+
+    final Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Handler mHandler = new Handler();
-
         btn1 = (Button) findViewById(R.id.btn1);
         btn2 = (Button) findViewById(R.id.btn2);
         txt1 = (TextView) findViewById(R.id.txt1);
         txt2 = (TextView) findViewById(R.id.txt2);
 
-        final String remoteIP = "192.168.11.255";
+        mSurfaceView = (SurfaceView) findViewById(R.id.touch);
+
+        final String remoteIP = "192.168.43.255";
         final int portNum = 12345;
 
         // Button1 がクリックされた時に呼び出されるコールバックを登録
         btn1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 n++;
-                // 文字列を作る
-                mBuffer.append(n);
-                txt1.setText(mBuffer.toString());  // tv には final が必要
-                mBuffer.delete(0, 99);
-                //toneGenerator.startTone(ToneGenerator.TONE_PROP_BEEP);
+                txt1.setText(n + "");
 
-                // UDP　送信
-                (new Thread(new Runnable() {
-                    public void run() {
-                        try {
-                            InetAddress host = InetAddress.getByName(remoteIP);
-                            //String message = "send by Android " + n + " \n";  // 送信メッセージ
-                            String message = "100 100";  // 送信メッセージ
-                            mSocket = new DatagramSocket();  //DatagramSocket 作成
-                            byte[] data = message.getBytes();
-                            mPacket = new DatagramPacket(data, data.length, host, portNum);  //DatagramPacket 作成
-                            mSocket.send(mPacket);
-                            mHandler.post(new Runnable() {
-                                @Override
-                                public void run() {
-                                    txt2.setText("送信完了しました");
-                                }
-                            });
-                        } catch (Exception e) {
-                            System.err.println("Exception : " + e);
-                            txt2.setText("送信失敗しました");
-                        }
-                    }
-                })).start();
+                sendMessage(remoteIP, portNum, 100, 200);
+
             }
         });
 
@@ -79,10 +63,16 @@ public class MainActivity extends ActionBarActivity {
         btn2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 n--;
-                mBuffer.append(n);
-                txt1.setText(mBuffer.toString());
-                mBuffer.delete(0, 99);
-                //toneGenerator.startTone(ToneGenerator.TONE_PROP_BEEP);
+                txt1.setText(n + "");
+            }
+        });
+
+        mSurfaceView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                txt1.setText((int) event.getX() + ":" + (int) event.getY());
+                sendMessage(remoteIP, portNum, (int) event.getX(), (int) event.getY());
+                return true;
             }
         });
 
@@ -95,7 +85,7 @@ public class MainActivity extends ActionBarActivity {
             mPacket = new DatagramPacket(data, data.length, host, portNum);  //DatagramPacket 作成
             txt2.setText("初期化が完了しました");
         } catch (Exception e) {
-            System.err.println("Exception : " + e);
+            err.println("Exception : " + e);
             txt2.setText("初期化に失敗しました");
         }
     }
@@ -120,5 +110,43 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //intをbyte-arrayに変える
+    byte[] intToByteArray(int a) {
+        return new byte[]{
+                (byte) ((a >> 24) & 0xFF), (byte) ((a >> 16) & 0xFF), (byte) ((a >> 8) & 0xFF), (byte) (a & 0xFF)
+        };
+    }
+
+    void sendMessage(final String ip, final int port, final int x, final int y) {
+        // UDP　送信
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    InetAddress host = InetAddress.getByName(ip);
+                    mSocket = new DatagramSocket();  //DatagramSocket 作成
+
+                    byte[] tmpArray;
+                    tmpArray = intToByteArray(x);
+                    arraycopy(tmpArray, 0, sendBuffer, 0, 4);
+                    tmpArray = intToByteArray(y);
+                    System.arraycopy(tmpArray, 0, sendBuffer, 4, 4);
+
+                    mPacket = new DatagramPacket(sendBuffer, sendBuffer.length, host, port);  //DatagramPacket 作成
+                    mSocket.send(mPacket);
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            txt2.setText("送信完了しました");
+                        }
+                    });
+                } catch (Exception e) {
+                    err.println("Exception : " + e);
+                    txt2.setText("送信失敗しました");
+                }
+            }
+        }).start();
+
     }
 }
